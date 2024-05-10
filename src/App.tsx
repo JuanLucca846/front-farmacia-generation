c39d2ed88b81677a4ba4ae7871794973627ddff2
JuanLucca846 <lucca470@gmail.com>
2024-05-10 10:35:31 -0300

    Arrumando Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
+import Home from "./pages/home/Home";
 import ListaCategorias from "./components/categorias/listaCategorias/ListaCategorias";
 
 function App() {
